78f99d31a0df788db1ab296d9d6b89977c9351fa
Alex Newman <posix4e@apache.org>
2013-06-21 00:28:05 -0700

    Added ohmdb copyright and lic.

--- ohmdb-client/src/test/java/ohmdb/TestProto.java
+++ ohmdb-client/src/test/java/ohmdb/TestProto.java
@@ -1,15 +1,34 @@
-package ohmdb;
-
-import ohmdb.client.generated.ClientProtos;
-import org.junit.Test;
+/*
+ * Copyright (C) 2013  Ohm Data
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  This file incorporates work covered by the following copyright and
+ *  permission notice:
+ */
 
 /**
  * Created with IntelliJ IDEA.
  * User: alex
  * Date: 5/22/13
  * Time: 12:26 AM
- * To change this template use File | Settings | File Templates.
  */
+package ohmdb;
+
+import ohmdb.client.generated.ClientProtos;
+import org.junit.Test;
+
 public class TestProto {
 
 
