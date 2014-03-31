04a10daca353f7b5909be05cf0454fdb1d618efb
Ryan Rawson <ryanobjc@gmail.com>
2014-03-31 11:28:02 -0700

    package info for interfaces skeleton

--- c5db/src/main/java/c5db/interfaces/package-info.java
+++ c5db/src/main/java/c5db/interfaces/package-info.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright (C) 2014  Ohm Data
  *
@@ -15,23 +16,3 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package c5db.interfaces;
-
-import c5db.messages.generated.ModuleType;
-import com.google.common.util.concurrent.Service;
-
-/**
- * An internal module. Is a guava service.
- * <p/>
- * An internal module is a component that can be started/stopped, and is the official
- * internal interface between different modules.
- * <p/>
- * TODO module dependencies so if you stop one module, you have to stop the dependencies.
- */
-public interface C5Module extends Service {
-
-    public ModuleType getModuleType();
-
-    public boolean hasPort();
-
-    public int port();
-}
