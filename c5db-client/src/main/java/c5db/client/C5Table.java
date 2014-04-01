51768d7c7be13c6fcd6dd3096fcc0b3701032e42
Alex Newman <posix4e@gmail.com>
2014-03-31 17:37:06 -0700

    C5-102. implement CheckAndPut and CheckAndDelete

--- c5db-client/src/main/java/c5db/client/C5Table.java
+++ c5db-client/src/main/java/c5db/client/C5Table.java
@@ -16,10 +16,14 @@
  */
 package c5db.client;
 
+import c5db.client.generated.ByteArrayComparable;
+import c5db.client.generated.CompareType;
+import c5db.client.generated.Condition;
 import c5db.client.generated.GetRequest;
 import c5db.client.generated.GetResponse;
 import c5db.client.generated.MultiRequest;
 import c5db.client.generated.MutateRequest;
+import c5db.client.generated.MutationProto;
 import c5db.client.generated.RegionAction;
 import c5db.client.generated.RegionSpecifier;
 import c5db.client.generated.Response;
@@ -31,16 +35,17 @@ import io.netty.channel.Channel;
 import io.netty.handler.codec.http.websocketx.CloseWebSocketFrame;
 import org.apache.hadoop.hbase.client.Delete;
 import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Mutation;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.RowMutations;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.mortbay.log.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.InterruptedIOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
@@ -54,17 +59,20 @@ import java.util.concurrent.atomic.AtomicLong;
  * The main client entry point for putting data into C5. Equivalent to HTablet from HBase.
  */
 public class C5Table extends C5Shim implements AutoCloseable {
+  private static final Logger LOG = LoggerFactory.getLogger(C5Table.class);
   private final C5ConnectionManager c5ConnectionManager = new C5ConnectionManager();
   private final ClientScannerManager clientScannerManager = ClientScannerManager.INSTANCE;
   private final int port;
   private final AtomicLong commandId = new AtomicLong(0);
   private final Channel channel;
   private final String hostname;
+  private final byte[] tableName;
   private MessageHandler handler;
 
   public C5Table(ByteString tableName) throws IOException, InterruptedException, TimeoutException, ExecutionException {
     this(tableName, C5Constants.TEST_PORT);
   }
+
 
   /**
    * C5Table is the main entry points for clients of C5DB
@@ -73,7 +81,7 @@ public class C5Table extends C5Shim implements AutoCloseable {
    */
   public C5Table(ByteString tableName, int port)
       throws IOException, InterruptedException, TimeoutException, ExecutionException {
-    super(tableName);
+    this.tableName = tableName.toByteArray();
     this.hostname = "localhost";
     this.port = port;
     channel = c5ConnectionManager.getOrCreateChannel(this.hostname, this.port);
@@ -87,13 +95,9 @@ public class C5Table extends C5Shim implements AutoCloseable {
 
   @Override
   public Result get(final Get get) throws IOException {
-
-    final SettableFuture<Response> resultFuture = SettableFuture.create();
-    final GetRequest getRequest = RequestConverter.buildGetRequest(getRegionName(), get, false);
     try {
-      handler.call(ProtobufUtil.getGetCall(commandId.incrementAndGet(), getRequest), resultFuture, channel);
-      return ProtobufUtil.toResult(resultFuture.get(C5Constants.TIMEOUT, TimeUnit.MILLISECONDS).getGet().getResult());
-    } catch (InterruptedException | ExecutionException | TimeoutException e) {
+      return doGetResult(get);
+    } catch (RuntimeException e) {
       throw new IOException(e);
     }
   }
@@ -101,30 +105,31 @@ public class C5Table extends C5Shim implements AutoCloseable {
   @Override
   public Result[] get(List<Get> gets) throws IOException {
     //TODO Batch get
-    final List<Result> results = new ArrayList<>();
-    for (Get get : gets) {
-      results.add(this.get(get));
+    try {
+      return gets.stream().map(this::doGetResult).toArray(Result[]::new);
+    } catch (RuntimeException e) {
+      throw new IOException(e);
     }
-    return results.toArray(new Result[results.size()]);
   }
 
   @Override
   public boolean exists(final Get get) throws IOException {
-    final SettableFuture<Response> resultFuture = SettableFuture.create();
-    final GetRequest getRequest = RequestConverter.buildGetRequest(getRegionName(),
-        get,
-        true);
-
     try {
-      handler.call(ProtobufUtil.getGetCall(commandId.incrementAndGet(), getRequest), resultFuture, channel);
-      final GetResponse getResponse = resultFuture.get(C5Constants.TIMEOUT, TimeUnit.MILLISECONDS).getGet();
-      final c5db.client.generated.Result result = getResponse.getResult();
-      return result.getExists();
-    } catch (InterruptedException | ExecutionException | TimeoutException e) {
+      return doExists(get);
+    } catch (RuntimeException e) {
       throw new IOException(e);
     }
-
   }
+
+  @Override
+  public Boolean[] exists(List<Get> gets) throws IOException {
+    try {
+      return gets.stream().map(this::doExists).toArray(Boolean[]::new);
+    } catch (RuntimeException e) {
+      throw new IOException(e);
+    }
+  }
+
 
   @Override
   public ResultScanner getScanner(final Scan scan) throws IOException {
@@ -171,56 +176,38 @@ public class C5Table extends C5Shim implements AutoCloseable {
 
   @Override
   public void put(Put put) throws IOException {
-    doPut(put);
-  }
-
-  @Override
-  public Boolean[] exists(List<Get> gets) throws IOException {
-    //TODO Batch get
-    final List<Boolean> results = new ArrayList<>();
-    for (Get get : gets) {
-      results.add(this.exists(get));
-    }
-    return results.toArray(new Boolean[results.size()]);
-  }
-
-  private void doPut(Put put) throws InterruptedIOException {
-    final SettableFuture<Response> resultFuture = SettableFuture.create();
-    final MutateRequest mutateRequest = RequestConverter.buildMutateRequest(getRegionName(), put);
-
     try {
-      handler.call(ProtobufUtil.getMutateCall(commandId.incrementAndGet(), mutateRequest), resultFuture, channel);
-      resultFuture.get(C5Constants.TIMEOUT, TimeUnit.MILLISECONDS);
-    } catch (InterruptedException | ExecutionException | TimeoutException e) {
-      throw new InterruptedIOException(e.toString());
-    }
-  }
-
-
-  @Override
-  public void put(List<Put> puts) throws IOException {
-    for (Put put : puts) {
       doPut(put);
+    } catch (RuntimeException e) {
+      throw new IOException(e);
     }
   }
 
   @Override
   public void delete(Delete delete) throws IOException {
-    final SettableFuture<Response> resultFuture = SettableFuture.create();
-    final MutateRequest mutateRequest = RequestConverter.buildMutateRequest(getRegionName(), delete);
+    try {
+      doDelete(delete);
+    } catch (RuntimeException e) {
+      throw new IOException(e);
+    }
+  }
+
+  @Override
+  public void put(List<Put> puts) throws IOException {
 
     try {
-      handler.call(ProtobufUtil.getMutateCall(commandId.incrementAndGet(), mutateRequest), resultFuture, channel);
-      resultFuture.get(C5Constants.TIMEOUT, TimeUnit.MILLISECONDS);
-    } catch (InterruptedException | ExecutionException | TimeoutException e) {
+      puts.forEach(this::doPut);
+    } catch (RuntimeException e) {
       throw new IOException(e);
     }
   }
 
   @Override
   public void delete(List<Delete> deletes) throws IOException {
-    for (Delete delete : deletes) {
-      delete(delete);
+    try {
+      deletes.forEach(this::doDelete);
+    } catch (RuntimeException e) {
+      throw new IOException(e);
     }
   }
 
@@ -235,7 +222,8 @@ public class C5Table extends C5Shim implements AutoCloseable {
       handler.call(ProtobufUtil.getMultiCall(commandId.incrementAndGet(),
           new MultiRequest(regionActions)),
           resultFuture,
-          channel);
+          channel
+      );
       resultFuture.get(C5Constants.TIMEOUT, TimeUnit.MILLISECONDS);
     } catch (InterruptedException | IOException | ExecutionException | TimeoutException e) {
       throw new IOException(e);
@@ -249,8 +237,124 @@ public class C5Table extends C5Shim implements AutoCloseable {
       channel.closeFuture().sync();
       c5ConnectionManager.closeChannel(this.hostname, this.port);
     } catch (InterruptedException | ExecutionException | TimeoutException e) {
-      Log.warn(e);
+      LOG.error(e.toString());
     }
+  }
 
+  @Override
+  public boolean checkAndPut(byte[] row, byte[] family, byte[] qualifier, byte[] value, Put put)
+      throws IOException {
+    final SettableFuture<Response> resultFuture = SettableFuture.create();
+    ByteArrayComparable byteArrayComparable = new ByteArrayComparable(ByteBuffer.wrap(value));
+
+    final Condition condition = new Condition(ByteBuffer.wrap(row),
+        ByteBuffer.wrap(family),
+        ByteBuffer.wrap(qualifier),
+        CompareType.EQUAL,
+        ProtobufUtil.toComparator(byteArrayComparable));
+
+    MutateRequest mutateRequest = RequestConverter.buildMutateRequest(getRegionName(),
+        MutationProto.MutationType.PUT,
+        put,
+        condition);
+
+    try {
+      handler.call(ProtobufUtil.getMutateCall(commandId.incrementAndGet(), mutateRequest), resultFuture, channel);
+      Response result = resultFuture.get(C5Constants.TIMEOUT, TimeUnit.MILLISECONDS);
+      if (result.getMutate().getProcessed()) {
+        return true;
+      }
+    } catch (InterruptedException | ExecutionException | TimeoutException e) {
+      throw new IOException(e.toString());
+    }
+    return false;
+  }
+
+  @Override
+  public boolean checkAndDelete(byte[] row, byte[] family, byte[] qualifier, byte[] value, Delete delete)
+      throws IOException {
+    final SettableFuture<Response> resultFuture = SettableFuture.create();
+    ByteArrayComparable byteArrayComparable = new ByteArrayComparable(ByteBuffer.wrap(value));
+
+    final Condition condition = new Condition(ByteBuffer.wrap(row),
+        ByteBuffer.wrap(family),
+        ByteBuffer.wrap(qualifier),
+        CompareType.EQUAL,
+        ProtobufUtil.toComparator(byteArrayComparable));
+
+    MutateRequest mutateRequest = RequestConverter.buildMutateRequest(getRegionName(),
+        MutationProto.MutationType.DELETE,
+        delete,
+        condition);
+    try {
+      handler.call(ProtobufUtil.getMutateCall(commandId.incrementAndGet(), mutateRequest), resultFuture, channel);
+      Response result = resultFuture.get(C5Constants.TIMEOUT, TimeUnit.MILLISECONDS);
+      if (result.getMutate().getProcessed()) {
+        return true;
+      }
+    } catch (InterruptedException | ExecutionException | TimeoutException e) {
+      throw new IOException(e.toString());
+    }
+    return false;
+  }
+
+
+  private Result doGetResult(Get get) throws RuntimeException {
+    try {
+      return doGetResult(get, false);
+    } catch (IOException | InterruptedException | ExecutionException | TimeoutException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private Boolean doExists(Get get) throws RuntimeException {
+    try {
+      Result getResult = doGetResult(get, true);
+      return getResult.getExists();
+    } catch (IOException | InterruptedException | ExecutionException | TimeoutException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+
+  private Result doGetResult(Get get, boolean existsOnly)
+      throws RuntimeException, IOException, InterruptedException, ExecutionException, TimeoutException {
+    final SettableFuture<Response> resultFuture = SettableFuture.create();
+    final GetRequest getRequest;
+    getRequest = RequestConverter.buildGetRequest(getRegionName(), get, existsOnly);
+    handler.call(ProtobufUtil.getGetCall(commandId.incrementAndGet(), getRequest), resultFuture, channel);
+    GetResponse getResponse = resultFuture.get(C5Constants.TIMEOUT, TimeUnit.MILLISECONDS).getGet();
+    if (existsOnly) {
+      return ProtobufUtil.toResultExists(getResponse.getResult());
+    } else {
+      return ProtobufUtil.toResult(getResponse.getResult());
+    }
+  }
+
+  private void doMutation(Mutation mutation, MutationProto.MutationType type) throws RuntimeException {
+    final SettableFuture<Response> resultFuture = SettableFuture.create();
+
+    final MutateRequest mutateRequest = RequestConverter.buildMutateRequest(getRegionName(),
+        type,
+        mutation);
+    handler.call(ProtobufUtil.getMutateCall(commandId.incrementAndGet(), mutateRequest), resultFuture, channel);
+    try {
+      resultFuture.get(C5Constants.TIMEOUT, TimeUnit.MILLISECONDS);
+    } catch (InterruptedException | TimeoutException | ExecutionException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private void doPut(Mutation mutation) throws RuntimeException {
+    doMutation(mutation, MutationProto.MutationType.PUT);
+  }
+
+  private void doDelete(Mutation mutation) throws RuntimeException {
+    doMutation(mutation, MutationProto.MutationType.DELETE);
+  }
+
+
+  private byte[] getRegionName() {
+    return tableName;
   }
 }
