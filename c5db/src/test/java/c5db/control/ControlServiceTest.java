29e2e466fee97a845d0f6d223626869cb7e36948
Ryan Rawson <ryanobjc@gmail.com>
2014-05-01 02:06:12 -0700

    Vastly improve control service test

--- c5db/src/test/java/c5db/control/ControlServiceTest.java
+++ c5db/src/test/java/c5db/control/ControlServiceTest.java
@@ -16,23 +16,27 @@
  */
 package c5db.control;
 
-import c5db.interfaces.C5Module;
 import c5db.interfaces.C5Server;
 import c5db.interfaces.DiscoveryModule;
+import c5db.interfaces.discovery.NodeInfoReply;
 import c5db.interfaces.server.CommandRpcRequest;
 import c5db.messages.generated.CommandReply;
 import c5db.messages.generated.ModuleSubCommand;
 import c5db.messages.generated.ModuleType;
-import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.SettableFuture;
 import io.netty.channel.nio.NioEventLoopGroup;
+import org.hamcrest.Description;
 import org.jetlang.channels.MemoryRequestChannel;
 import org.jetlang.channels.Request;
 import org.jetlang.channels.RequestChannel;
+import org.jetlang.channels.Session;
 import org.jetlang.fibers.Fiber;
 import org.jetlang.fibers.PoolFiberFactory;
 import org.jetlang.fibers.ThreadFiber;
 import org.jmock.Expectations;
+import org.jmock.api.Action;
+import org.jmock.api.Invocation;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.jmock.lib.concurrent.Synchroniser;
 import org.junit.After;
@@ -70,7 +74,6 @@ public class ControlServiceTest {
   private final PoolFiberFactory fiberFactory = new PoolFiberFactory(Executors.newFixedThreadPool(2));
   private final C5Server server = context.mock(C5Server.class);
   private final DiscoveryModule discoveryModule = context.mock(DiscoveryModule.class);
-  private final SettableFuture<C5Module> getModuleFuture = SettableFuture.create();
 
   private ControlService controlService ;
   private SimpleControlClient controlClient;
@@ -82,7 +85,6 @@ public class ControlServiceTest {
 
   @Before
   public void before() {
-    getModuleFuture.set(discoveryModule);
     Random portRandomizer = new Random();
     modulePortUnderTest = 3000 + portRandomizer.nextInt(3000);
 
@@ -91,7 +93,7 @@ public class ControlServiceTest {
       will(returnValue(serverRequests));
 
       allowing(server).getModule(with(equal(ModuleType.Discovery)));
-      will(returnValue(getModuleFuture));
+      will(returnFutureWithValue(discoveryModule));
 
       allowing(server).getNodeId();
       will(returnValue(LOCAL_NODE_ID));
@@ -99,7 +101,6 @@ public class ControlServiceTest {
 
     ourFiber.start();
     serverRequests.subscribe(ourFiber, this::handleServerRequests);
-
 
     controlService = new ControlService(
         server,
@@ -110,6 +111,8 @@ public class ControlServiceTest {
     );
 
     controlClient = new SimpleControlClient(ioWorkerGroup);
+
+    controlService.startAndWait();
   }
 
   private final CommandReply serverReply = new CommandReply(true, DONT_CARE_REPLY_PAYLOAD, DONT_CARE_EMPTY);
@@ -132,13 +135,7 @@ public class ControlServiceTest {
 
   @Test(timeout = 3000)
   public void shouldOpenAHTTPSocketAndAcceptAValidRequest() throws UnknownHostException, InterruptedException, ExecutionException {
-    controlService.startAndWait();
-
-    CommandRpcRequest cmd = new CommandRpcRequest<>(LOCAL_NODE_ID, new ModuleSubCommand(ModuleType.Tablet, DONT_CARE_PAYLOAD));
-    ListenableFuture<CommandReply> future = controlClient.sendRequest(cmd,
-        new InetSocketAddress(InetAddress.getByName("localhost"), modulePortUnderTest));
-
-    CommandReply reply = future.get();
+    CommandReply reply = controlClient.sendRequest(rpcRequest(), moduleInetAddress()).get();
     //assertThat(reply, is(serverReply));
     // TODO make it so that protostuff messages compare via .equals properly
     assertThat(reply.getCommandStdout(), is(serverReply.getCommandStdout()));
@@ -147,15 +144,173 @@ public class ControlServiceTest {
 
   @Test(timeout = 3000)
   public void shouldReturnAnErrorWithInvalidNodeId() throws UnknownHostException, ExecutionException, InterruptedException {
-    controlService.startAndWait();
-
-    CommandRpcRequest cmd = new CommandRpcRequest<>(NOT_LOCAL_NODE_ID, new ModuleSubCommand(ModuleType.Tablet, DONT_CARE_PAYLOAD));
-    ListenableFuture<CommandReply> future = controlClient.sendRequest(cmd,
-        new InetSocketAddress(InetAddress.getByName("localhost"), modulePortUnderTest));
-
-    CommandReply reply = future.get();
+    CommandReply reply = controlClient.sendRequest(rpcRequestWithWrongNodeId(), moduleInetAddress()).get();
 
     assertThat(reply.getCommandSuccess(), is(false));
     System.out.println("reply is: " + reply);
   }
+
+  private InetSocketAddress moduleInetAddress() throws UnknownHostException {
+    return new InetSocketAddress(InetAddress.getByName("localhost"), modulePortUnderTest);
+  }
+
+  @Test(timeout = 3000)
+  public void shouldHaveEmbededClientCalledByDoMessageCallItself() throws ExecutionException, InterruptedException {
+    context.checking(new Expectations() {{
+      allowing(discoveryModule).getNodeInfo(LOCAL_NODE_ID, ModuleType.ControlRpc);
+      will(returnFutureWithValue(nodeInfo().withPort(modulePortUnderTest)));
+    }});
+
+    ReplyWaiter<CommandRpcRequest<?>, CommandReply> waiter = new ReplyWaiter<>(rpcRequest());
+
+    controlService.doMessage(waiter);
+
+    CommandReply reply = waiter.repliedFuture.get();
+
+    // TODO better compare
+    assertThat(reply.getCommandSuccess(), is(true));
+    assertThat(reply.getCommandStdout(), is(serverReply.getCommandStdout()));
+  }
+
+  @Test(timeout = 3000)
+  public void embededClientHandlesCantFindNode() throws ExecutionException, InterruptedException {
+    context.checking(new Expectations() {{
+      allowing(discoveryModule).getNodeInfo(LOCAL_NODE_ID, ModuleType.ControlRpc);
+      will(returnFutureWithValue(nodeInfo().failedToLookupNode()));
+    }});
+    ReplyWaiter<CommandRpcRequest<?>, CommandReply> waiter = new ReplyWaiter<>(rpcRequest());
+
+    controlService.doMessage(waiter);
+
+    CommandReply reply = waiter.repliedFuture.get();
+
+    assertThat(reply.getCommandSuccess(), is(false));
+    System.out.println(reply);
+  }
+
+  @Test(timeout = 3000)
+  public void embeddedClientBadDNSLoopup() throws ExecutionException, InterruptedException {
+    context.checking(new Expectations(){{
+      allowing(discoveryModule).getNodeInfo(LOCAL_NODE_ID, ModuleType.ControlRpc);
+      will(returnFutureWithValue(nodeInfo().withAddress("WEIRD_ADDRESS_DNS_FAIL_ME")));
+    }});
+    ReplyWaiter<CommandRpcRequest<?>, CommandReply> waiter = new ReplyWaiter<>(rpcRequest());
+
+    controlService.doMessage(waiter);
+
+    CommandReply reply = waiter.repliedFuture.get();
+
+    assertThat(reply.getCommandSuccess(), is(false));
+    System.out.println(reply);
+  }
+
+  @Test
+  public void embeddedClientWasGivenAnIncorrectPort() throws ExecutionException, InterruptedException {
+    context.checking(new Expectations() {{
+      allowing(discoveryModule).getNodeInfo(LOCAL_NODE_ID, ModuleType.ControlRpc);
+      will(returnFutureWithValue(nodeInfo().withPort(modulePortUnderTest+1)));
+    }});
+    ReplyWaiter<CommandRpcRequest<?>, CommandReply> waiter = new ReplyWaiter<>(rpcRequest());
+
+    controlService.doMessage(waiter);
+
+    CommandReply reply = waiter.repliedFuture.get();
+
+    assertThat(reply.getCommandSuccess(), is(false));
+    System.out.println(reply);
+  }
+
+  public static ModuleSubCommand subCommand() {
+    return new ModuleSubCommand(ModuleType.Tablet, DONT_CARE_PAYLOAD);
+  }
+
+  public static CommandRpcRequest<ModuleSubCommand> rpcRequest() {
+    return new CommandRpcRequest<>(LOCAL_NODE_ID, subCommand());
+  }
+
+  public static CommandRpcRequest<ModuleSubCommand> rpcRequestWithWrongNodeId() {
+    return new CommandRpcRequest<>(NOT_LOCAL_NODE_ID, subCommand());
+  }
+
+  public static class ReplyWaiter<R, V> implements Request<R, V> {
+
+    private final R requestValue;
+    public SettableFuture<V> repliedFuture = SettableFuture.create();
+
+    public ReplyWaiter(R requestValue) {
+
+      this.requestValue = requestValue;
+    }
+
+    @Override
+    public Session getSession() {
+      return null;
+    }
+
+    @Override
+    public R getRequest() {
+      return requestValue;
+    }
+
+    @Override
+    public void reply(V repliedMessage) {
+      repliedFuture.set(repliedMessage);
+    }
+  }
+
+  public static NodeInfoReplyBuilder nodeInfo() {
+    return new NodeInfoReplyBuilder();
+  }
+
+  public static class NodeInfoReplyBuilder {
+    private boolean successFlag = true;
+    private String address = "localhost";
+    private int port;
+
+    public NodeInfoReplyBuilder withPort(int port) {
+      this.port = port;
+      return this;
+    }
+
+    public NodeInfoReplyBuilder withAddress(String address) {
+      this.address = address;
+      return this;
+    }
+
+    public NodeInfoReplyBuilder failedToLookupNode() {
+      successFlag = false;
+      return this;
+    }
+
+    NodeInfoReply build() {
+      return new NodeInfoReply(successFlag, Lists.newArrayList(address), port);
+    }
+  }
+
+  public static Action returnFutureWithValue(NodeInfoReplyBuilder builder) {
+    return returnFutureWithValue(builder.build());
+  }
+
+  public static Action returnFutureWithValue(Object futureValue) {
+    return new ReturnFutureWithValueAction(futureValue);
+  }
+
+  public static class ReturnFutureWithValueAction implements Action {
+    private Object futureResult;
+    public ReturnFutureWithValueAction(Object futureResult) {
+      this.futureResult = futureResult;
+    }
+
+    public Object invoke(Invocation invocation) {
+      SettableFuture<Object> future = SettableFuture.create();
+      future.set(futureResult);
+      return future;
+    }
+    public void describeTo(Description description) {
+      description.appendText("returns a future with value ");
+      description.appendValue(futureResult);
+    }
+  }
+
+
 }
