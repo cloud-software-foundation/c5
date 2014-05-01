cf032584ef13b5d07219abf636d1023569654ffb
Alex Newman <posix4e@apache.org>
2014-05-01 13:40:12 -0700

    Shrink the size of the TestFlush
    	Also move MiniCluster to a temp directory

--- c5db/src/main/java/c5db/C5ServerConstants.java
+++ c5db/src/main/java/c5db/C5ServerConstants.java
@@ -51,4 +51,5 @@ public class C5ServerConstants {
   public static final java.lang.String MIN_CLUSTER_SIZE = "minClusterSize";
   public static final int MINIMUM_DEFAULT_QUORUM_SIZE = 3;
   public static final int DEFAULT_QUORUM_SIZE = 1;
+  public static final String C5_CFG_PATH = "c5.cfgPath";
 }
