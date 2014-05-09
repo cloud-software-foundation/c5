3e90e6325abbacfb7bfdcd1ed02ef6601eb63783
Alex Newman <posix4e@apache.org>
2014-05-09 16:53:31 -0700

    A small nit

--- c5db/src/main/java/c5db/tablet/MetaTabletLeaderBehavior.java
+++ c5db/src/main/java/c5db/tablet/MetaTabletLeaderBehavior.java
@@ -16,11 +16,7 @@
  */
 package c5db.tablet;
 
-import c5db.C5DB;
 import c5db.C5ServerConstants;
-import c5db.client.generated.RegionInfo;
-import c5db.client.generated.TableName;
-import c5db.control.ControlService;
 import c5db.interfaces.C5Module;
 import c5db.interfaces.C5Server;
 import c5db.interfaces.ControlModule;
@@ -29,71 +25,54 @@ import c5db.interfaces.tablet.Tablet;
 import c5db.messages.generated.CommandReply;
 import c5db.messages.generated.ModuleSubCommand;
 import c5db.messages.generated.ModuleType;
-import c5db.util.FiberOnly;
 import com.google.common.util.concurrent.ListenableFuture;
-import io.protostuff.LinkedBuffer;
-import io.protostuff.ProtobufIOUtil;
-import org.apache.commons.lang.StringUtils;
-import org.apache.hadoop.hbase.HConstants;
-import org.apache.hadoop.hbase.client.Get;
-import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.client.Result;
-import org.jetlang.channels.Channel;
 import org.jetlang.channels.Request;
 import org.jetlang.channels.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class MetaTabletLeaderBehavior implements TabletLeaderBehavior {
 
   private static final Logger LOG = LoggerFactory.getLogger(MetaTabletLeaderBehavior.class);
-  private final Tablet tablet;
-  private final C5Server server;
-
+  private final ListenableFuture<C5Module> f;
+  private final  Request<CommandRpcRequest<?>, CommandReply> request;
 
   public MetaTabletLeaderBehavior(final Tablet tablet, final C5Server server) {
-    this.tablet = tablet;
-    this.server = server;
+    String metaLeader = C5ServerConstants.SET_META_LEADER + " : " + server.getNodeId();
+    ModuleSubCommand moduleSubCommand = new ModuleSubCommand(ModuleType.Tablet, metaLeader);
+    CommandRpcRequest<ModuleSubCommand> commandCommandRpcRequest
+        = new CommandRpcRequest<>(tablet.getLeader(), moduleSubCommand);
+
+    request = new Request<CommandRpcRequest<?>, CommandReply>() {
+      @Override
+      public Session getSession() {
+        LOG.info("getSession");
+        return null;
+      }
+
+      @Override
+      public CommandRpcRequest<ModuleSubCommand> getRequest() {
+        return commandCommandRpcRequest;
+      }
+
+      @Override
+      public void reply(CommandReply i) {
+        LOG.info("Command Reply");
+      }
+    };
+    f = server.getModule(ModuleType.ControlRpc);
   }
 
   public void start() {
-    setMetaLeaderInRootTablet();
-  }
-
-  private void setMetaLeaderInRootTablet() {
-    CommandRpcRequest<ModuleSubCommand> commandCommandRpcRequest
-        = new CommandRpcRequest<>(this.tablet.getLeader(),
-        new ModuleSubCommand(ModuleType.Tablet, C5ServerConstants.SET_META_LEADER + " : " + server.getNodeId()));
-    ListenableFuture<C5Module> f = server.getModule(ModuleType.ControlRpc);
+    ControlModule controlService = null;
     try {
-      ControlModule controlService = (ControlModule) f.get();
-      controlService.doMessage(new Request<CommandRpcRequest<?>, CommandReply>() {
-        @Override
-        public Session getSession() {
-          LOG.info("getSession");
-          return null;
-        }
-
-        @Override
-        public CommandRpcRequest<?> getRequest() {
-          return commandCommandRpcRequest;
-        }
-
-        @Override
-        public void reply(CommandReply i) {
-          LOG.info("Command Reply");
-        }
-      });
+      controlService = (ControlModule) f.get();
     } catch (InterruptedException | ExecutionException e) {
       System.exit(1);
       e.printStackTrace();
     }
+    controlService.doMessage(request);
   }
 }
