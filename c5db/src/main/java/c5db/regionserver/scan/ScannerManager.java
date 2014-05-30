592eada7e951d4063f8d6168d9ad6fbc8475a3b9
Alex Newman <posix4e@apache.org>
2014-05-30 13:30:37 -0700

    Isolate scan logic into it's own package

--- c5db/src/main/java/c5db/regionserver/scan/ScannerManager.java
+++ c5db/src/main/java/c5db/regionserver/scan/ScannerManager.java
@@ -15,7 +15,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package c5db.regionserver;
+package c5db.regionserver.scan;
 
 import org.jetlang.channels.Channel;
 
