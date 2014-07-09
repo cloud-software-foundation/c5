0ff757602a835ae3cc3f829e9034046d43349e15
Joshua Greenberg <josh@ohmdata.com>
2014-07-09 04:56:57 -0400

    Stub out general replicator test + fixup 2 passing

--- c5db/src/test/java/c5db/replication/GeneralReplicatorTest.java
+++ c5db/src/test/java/c5db/replication/GeneralReplicatorTest.java
@@ -33,13 +33,17 @@ import c5db.interfaces.replication.ReplicatorInstanceEvent;
 import c5db.log.LogService;
 import c5db.log.LogTestUtil;
 import c5db.messages.generated.ModuleType;
+import c5db.util.C5Futures;
 import c5db.util.ExceptionHandlingBatchExecutor;
+import c5db.util.FiberOnly;
 import c5db.util.JUnitRuleFiberExceptions;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.AbstractService;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.Service;
+import com.google.common.util.concurrent.SettableFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import org.jetlang.channels.Channel;
@@ -49,11 +53,16 @@ import org.jetlang.fibers.Fiber;
 import org.jetlang.fibers.PoolFiberFactory;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
@@ -67,8 +76,10 @@ import static c5db.C5ServerConstants.DISCOVERY_PORT;
 import static c5db.C5ServerConstants.REPLICATOR_PORT_MIN;
 import static c5db.interfaces.replication.ReplicatorInstanceEvent.EventType.LEADER_ELECTED;
 import static c5db.replication.ReplicatorService.FiberFactory;
+import static org.hamcrest.Matchers.equalTo;
 
 public class GeneralReplicatorTest {
+  @Rule
   public JUnitRuleFiberExceptions jUnitFiberExceptionHandler = new JUnitRuleFiberExceptions();
 
   private static final int NUMBER_OF_PROCESSORS = Runtime.getRuntime().availableProcessors();
@@ -79,14 +90,14 @@ public class GeneralReplicatorTest {
 
   private final PoolFiberFactory fiberFactory = new PoolFiberFactory(executorService);
   private final Set<Fiber> fibers = new HashSet<>();
-  private final Fiber testFiber = newTestFiber(jUnitFiberExceptionHandler);
+  private final Fiber mainTestFiber = newExceptionHandlingFiber(jUnitFiberExceptionHandler);
 
   private ConfigDirectory configDirectory;
 
   @Before
   public void setupConfigDirectory() throws Exception {
     configDirectory = new NioFileConfigDirectory(new C5CommonTestUtil().getDataTestDir("general-replicator-test"));
-    testFiber.start();
+    mainTestFiber.start();
   }
 
   @After
@@ -102,15 +113,15 @@ public class GeneralReplicatorTest {
     workerGroup.terminationFuture().get();
   }
 
-  @Test(timeout = 3000)
+  @Test(timeout = 9000)
   public void establishesASystemForGenericReplicationOfDataAcrossNodes() throws Exception {
-    ReplicationServer replicationServer = new ReplicationServer(1, REPLICATOR_PORT_MIN, DISCOVERY_PORT, this::newTestFiber);
-    replicationServer.start();
+    ReplicationServer replicationServer = new ReplicationServer(1, REPLICATOR_PORT_MIN, DISCOVERY_PORT, this::newExceptionHandlingFiber);
+    replicationServer.startAndWait();
 
     Replicator replicator = replicationServer.createReplicator("quorumId", Lists.newArrayList(1L)).get();
 
-    ChannelHistoryMonitor<ReplicatorInstanceEvent> eventMonitor = new ChannelHistoryMonitor<>(replicator.getEventChannel(), testFiber);
-    ChannelHistoryMonitor<IndexCommitNotice> commitMonitor = new ChannelHistoryMonitor<>(replicator.getCommitNoticeChannel(), testFiber);
+    ChannelHistoryMonitor<ReplicatorInstanceEvent> eventMonitor = new ChannelHistoryMonitor<>(replicator.getEventChannel(), mainTestFiber);
+    ChannelHistoryMonitor<IndexCommitNotice> commitMonitor = new ChannelHistoryMonitor<>(replicator.getCommitNoticeChannel(), mainTestFiber);
 
     replicator.start();
 
@@ -118,34 +129,53 @@ public class GeneralReplicatorTest {
 
     replicator.logData(Lists.newArrayList(LogTestUtil.someData())).get();
 
-    commitMonitor.waitFor(IndexCommitMatcher.aCommitNotice());
+    commitMonitor.waitFor(IndexCommitMatcher.aCommitNotice().withIndex(equalTo(1L)));
 
-    replicationServer.close();
+    replicationServer.stopAndWait();
+    replicationServer.dispose();
   }
 
-  private Fiber newTestFiber(Consumer<Throwable> throwableHandler) {
+  private Fiber newExceptionHandlingFiber(Consumer<Throwable> throwableHandler) {
     Fiber newFiber = fiberFactory.create(new ExceptionHandlingBatchExecutor(throwableHandler));
     fibers.add(newFiber);
     return newFiber;
   }
 
-  private class SynchronousModuleServer implements ModuleServer {
+  /**
+   * Stand-in for C5Server; coordinates the interaction of the local modules
+   */
+  private class SimpleModuleServer implements ModuleServer {
+    private final Fiber fiber;
     private final Map<ModuleType, C5Module> modules = new HashMap<>();
     private final Map<ModuleType, Integer> modulePorts = new HashMap<>();
     private final Channel<ImmutableMap<ModuleType, Integer>> modulePortsChannel = new MemoryChannel<>();
 
-    public void addAndStartModule(C5Module module) throws InterruptedException, ExecutionException {
-      module.addListener(new SimpleModuleListener(module), testFiber);
-      module.start().get();
+    private SimpleModuleServer(Fiber fiber) {
+      this.fiber = fiber;
+    }
 
+    public ListenableFuture<Service.State> startModule(C5Module module) {
+      SettableFuture<Service.State> startedFuture = SettableFuture.create();
+      Service.Listener stateChangeListener = new SimpleModuleListener(
+          module,
+          () -> {
+            addRunningModule(module);
+            startedFuture.set(null);
+          },
+          () -> removeModule(module));
+
+      module.addListener(stateChangeListener, fiber);
       modules.put(module.getModuleType(), module);
-      modulePorts.put(module.getModuleType(), module.port());
-      modulePortsChannel.publish(ImmutableMap.copyOf(modulePorts));
+      module.start();
+
+      return startedFuture;
     }
 
     @Override
     public ListenableFuture<C5Module> getModule(ModuleType moduleType) {
-      return Futures.immediateFuture(modules.get(moduleType));
+      SettableFuture<C5Module> moduleFuture = SettableFuture.create();
+      fiber.execute(() -> moduleFuture.set(modules.get(moduleType)));
+      return moduleFuture;
     }
 
     @Override
@@ -158,68 +188,161 @@ public class GeneralReplicatorTest {
         throws ExecutionException, InterruptedException, TimeoutException {
       throw new UnsupportedOperationException();
     }
+
+    @FiberOnly
+    private void addRunningModule(C5Module module) {
+      ModuleType type = module.getModuleType();
+      if (modules.containsKey(type) && modules.get(type).equals(module)) {
+        modulePorts.put(type, module.port());
+        publishCurrentActivePorts();
+      }
+    }
+
+    @FiberOnly
+    private void removeModule(C5Module module) {
+      ModuleType type = module.getModuleType();
+      if (modules.containsKey(type) && modules.get(type).equals(module)) {
+        modules.remove(type);
+        modulePorts.remove(type);
+        publishCurrentActivePorts();
+      }
+    }
+
+    @FiberOnly
+    private void publishCurrentActivePorts() {
+      modulePortsChannel.publish(ImmutableMap.copyOf(modulePorts));
+    }
   }
 
-  private class ReplicationServer implements AutoCloseable {
-    private final SynchronousModuleServer moduleServer = new SynchronousModuleServer();
+  /**
+   * Replication server that includes its own discovery and logging mechanism; internally
+   * it bundles a ReplicationModule, a LogModule, and a DiscoveryModule. It then implements
+   * the ReplicationModule interface and delegates to its internal ReplicationModule.
+   */
+  private class ReplicationServer extends AbstractService implements ReplicationModule {
+    private final Fiber serverFiber;
+    private final SimpleModuleServer moduleServer;
     private final Fiber discoveryFiber;
     private final DiscoveryModule discoveryModule;
     private final LogModule logModule = new LogService(configDirectory);
     private final ReplicationModule replicationModule;
 
+    private final int replicatorPort;
+
     public ReplicationServer(long nodeId, int replicatorPort, int discoveryPort, FiberFactory fiberFactory) {
-      // BeaconService will start this Fiber on its own
+      this.replicatorPort = replicatorPort;
+
+      serverFiber = fiberFactory.getFiber(jUnitFiberExceptionHandler);
+      moduleServer = new SimpleModuleServer(serverFiber);
+      serverFiber.start();
+
+      // TODO BeaconService will start this Fiber on its own, but it probably should be started by us
       discoveryFiber = fiberFactory.getFiber(jUnitFiberExceptionHandler);
       discoveryModule = new BeaconService(nodeId, discoveryPort, discoveryFiber, workerGroup, ImmutableMap.of(), moduleServer);
+
+      // TODO ReplicatorService provides no way to dispose of its own fiber; it should
       replicationModule = new ReplicatorService(bossGroup, workerGroup, nodeId, replicatorPort, moduleServer, fiberFactory, configDirectory);
     }
 
-    public void start() throws InterruptedException, ExecutionException {
-      moduleServer.addAndStartModule(logModule);
-      moduleServer.addAndStartModule(discoveryModule);
-      moduleServer.addAndStartModule(replicationModule);
+    @Override
+    protected void doStart() {
+      List<ListenableFuture<Service.State>> startFutures = new ArrayList<>();
+
+      startFutures.add(moduleServer.startModule(logModule));
+      startFutures.add(moduleServer.startModule(discoveryModule));
+      startFutures.add(moduleServer.startModule(replicationModule));
+
+      ListenableFuture<List<Service.State>> allFutures = Futures.allAsList(startFutures);
+
+      C5Futures.addCallback(allFutures,
+          (List<Service.State> ignore) -> notifyStarted(),
+          this::notifyFailed,
+          serverFiber);
     }
 
+    @Override
+    protected void doStop() {
+      replicationModule.stopAndWait();
+      discoveryModule.stopAndWait();
+      logModule.stopAndWait();
+
+      notifyStopped();
+    }
+
+    @Override
     public ListenableFuture<Replicator> createReplicator(String quorumId, Collection<Long> peerIds) {
       return replicationModule.createReplicator(quorumId, peerIds);
     }
 
     @Override
-    public void close() {
-      replicationModule.stopAndWait();
-      discoveryModule.stopAndWait();
-      logModule.stopAndWait();
+    public ModuleType getModuleType() {
+      return ModuleType.Replication;
+    }
 
-      discoveryFiber.dispose();
+    @Override
+    public boolean hasPort() {
+      return true;
+    }
+
+    @Override
+    public int port() {
+      return replicatorPort;
+    }
+
+    @Override
+    public String acceptCommand(String commandString) throws InterruptedException {
+      throw new UnsupportedOperationException();
+    }
+
+    public void dispose() {
+      serverFiber.dispose();
+
     }
   }
 
+  /**
+   * A {@link com.google.common.util.concurrent.Service.Listener} that logs the module lifecycle
+   * and performs a single action when the module stops or fails.
+   */
   private class SimpleModuleListener implements Service.Listener {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final C5Module module;
+    private final Runnable removeModule;
+    private final Runnable runningModule;
 
-    private SimpleModuleListener(C5Module module) {
+    private SimpleModuleListener(C5Module module, Runnable runningModule, Runnable removeModule) {
       this.module = module;
+      this.removeModule = removeModule;
+      this.runningModule = runningModule;
     }
 
     @Override
     public void starting() {
+      logger.info("Started module {}", module);
     }
 
     @Override
     public void running() {
+      logger.info("Running module {}", module);
+      runningModule.run();
     }
 
     @Override
     public void stopping(Service.State from) {
+      logger.info("Stopping module {}", module);
+      removeModule.run();
     }
 
     @Override
     public void terminated(Service.State from) {
+      logger.info("Terminated module {}", module);
+      removeModule.run();
     }
 
     @Override
     public void failed(Service.State from, Throwable failure) {
-      throw new AssertionError("Failure in module " + module + ": " + failure);
+      logger.error("Failed module {}: {}", module, failure);
+      removeModule.run();
     }
   }
 }
